e452667697a1a4ddd14c6855bc51532aaf79b313
Edward Velasco <edwvelasco@deloitte.com>
2023-01-20 23:34:28 +0800

    added API Request

--- index.js
+++ index.js
@@ -3,8 +3,34 @@ const express = require('express');
 const app = express();
 const exphbs = require('express-handlebars');
 const path = require('path');
+const request = require('request');
 
 const PORT = process.env.PORT || 5000;
+
+
+
+// API Key pk_8e5fb969df5b484da5e2fc52a592841a
+// Secret sk_454a8faa419d4684b40dc070f6648a9c
+// Create call_api function
+function call_api(finishedAPI) {
+	request('https://cloud.iexapis.com/v1/stock/aapl/quote/latestPrice?token=pk_8e5fb969df5b484da5e2fc52a592841a', { json: true }, (err, res, body) => {
+	if (err) {return console.log(err);}
+	if (res.statusCode === 200){
+		// console.log(body);
+		finishedAPI(body);
+	};
+});
+};
+
+// request('https://cloud.iexapis.com/v1/stock/aapl/quote/latestPrice?token=pk_8e5fb969df5b484da5e2fc52a592841a', { json: true }, (err, res, body) => {
+// 	if (err) {return console.log(err);}
+// 	if (res.statusCode === 200){
+// 		 console.log(body);
+// 		//finishedAPI(body);
+// 	};
+// });
+
+
 
 // Set Handlebars Middleware
 app.engine('handlebars', exphbs.engine());
@@ -14,10 +40,17 @@ const otherstuff = "hello there, this is other stuff!";
 
 // Set handlebar routes
 app.get('/', function (req, res) {
-	res.render('home', {
-		stuff: otherstuff
+	call_api(function(doneAPI){
+		res.render('home', {
+		stock: doneAPI
+		});
 	});
+	
+});
 
+// Set about page routes
+app.get('/about.html', function (req, res) {
+	res.render('about');
 });
 
 
